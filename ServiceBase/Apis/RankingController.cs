e78c3bdf22a80cea3d0beacc240c580e48c3f013
이진영 <codingzombie82@gmail.com>
2021-01-19 22:34:42 +0900

    테스트 코드

---
+++ ServiceBase/Apis/RankingController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceBase.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankingController : ControllerBase
+    {
+        //개체 형식
+        //[HttpGet]
+        //public Subject Get()
+        //{
+        //    return new Subject { kor = 95, Eng = 100, Total =  195};
+        //}
+
+        //컬렉션 형식
+        //[HttpGet]
+        //public IEnumerable<Student> Get()
+        //{
+        //    return new List<Student> {
+        //        new Student{Id = 0, Name ="홍길동", Score = 1},
+        //        new Student{Id = 1, Name ="이길동", Score = 2},
+        //        new Student{Id = 3, Name ="삼길동", Score = 3}
+        //    };
+        //}
+
+        //복합형식
+        [HttpGet]
+        public RankingDto Get()
+        {
+            var subject = new Subject { kor = 95, Eng = 100, Total = 195 };
+            var students =  new List<Student> {
+                new Student{Id = 0, Name ="홍길동", Score = 1},
+                new Student{Id = 1, Name ="이길동", Score = 2},
+                new Student{Id = 3, Name ="삼길동", Score = 3}
+            };
+
+            var rankings = new RankingDto
+            {
+                Subject = subject,
+                Students = students
+            };
+
+            return rankings;
+        }
+    }
+
+    public class Subject
+    {
+        public int kor { get; set; }
+        public int Eng { get; set; }
+        public int Total { get; set; }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+
+    public class RankingDto
+    {
+        public Subject Subject { get; set; }
+        public List<Student> Students { get; set; }
+
+    }
+}
--- ServiceBase/Startup.cs
+++ ServiceBase/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using ServiceBase.Controllers;
 using ServiceBase.Data;
 using ServiceBase.Services;
 using System;
@@ -37,36 +38,55 @@ namespace ServiceBase
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            //JWT 인증 사용
             services.AddAuthentication(options =>
-             {
-                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-             }).AddJwtBearer(cfg =>
-             {
-                  cfg.RequireHttpsMetadata = true;
-                  cfg.SaveToken = true;
-                  cfg.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
-                  {
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(cfg =>
+            {
+                 cfg.RequireHttpsMetadata = true;
+                 cfg.SaveToken = true;
+                 cfg.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                 {
                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("placeholder-key-that-is-long-enough-for-sha256")),
-                      ValidateAudience = false,
-                      ValidateIssuer = false,
-                      ValidateLifetime = false,
-                      RequireExpirationTime = false,
-                      ClockSkew = TimeSpan.Zero,
-                      ValidateIssuerSigningKey = true
-                  };
-             });
+                      ValidateAudience = false, //토큰 유효성 검사 중에 대상의 유효성을 검사할지 여부
+                      ValidateIssuer = false, //토큰 유효성 검사 중에 발급자가 유효성을 검사할지 여부
+                      ValidateLifetime = false, //토큰 유효성 검사 중에 수명의 유효성을 검사할지 여부
+                      RequireExpirationTime = false, //토큰에 만료시간 속성이 필요한지 적용
+                      ClockSkew = TimeSpan.Zero, //시간의 유효성을 검사 할 때 적용 
+                      ValidateIssuerSigningKey = true //securityToken 에 서명 한 SecurityKey의 유효성 검사 가 호출 되는지 여부
+                 };
+            });
+            services.AddScoped<ITokenBuilder, TokenBuilder>(); // 토큰 생성
+            //JWT 인증 사용
 
 
+            services.AddControllers(); // MVC를 사용하기 위해
 
-
-            services.AddScoped<ITokenBuilder, TokenBuilder>();
-
-            services.AddControllers();
-
+            //블레이저 사용
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            //블레이저 사용
+
             services.AddSingleton<WeatherForecastService>();
+
+           
+            //[DI] 의존성 주입(Dependency Injection)
+            DependencyInjectionContainer(services);
+        }
+
+        /// <summary>
+        /// 의존성 주입 관련 코드만 따로 모아서 관리
+        /// - 리포지토리 등록
+        /// </summary>
+        private void DependencyInjectionContainer(IServiceCollection services)
+        { 
+            //테스트용
+            services.AddTransient<IPointRepository, PointRepositoryInMemory>(); // 인-메모리 사용
+            services.AddTransient<IPointLogRepository, PointLogRepository>();
+            // Exams: Questions, ... 
+            services.AddTransient<IQuestionRepository, QuestionRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -88,18 +108,23 @@ namespace ServiceBase
 
             app.UseRouting();
 
-
-            app.UseAuthentication();
-            app.UseAuthorization();
+            app.UseAuthentication(); // 토큰 인증 후 Api 사용 인증에 대한 처리 [Authorize]
+            app.UseAuthorization(); //인증 
 
             app.UseEndpoints(endpoints =>
             {
+                //MVC 라우터
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                //MVC 라우터
+
+                //블레이저 기본
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
                 endpoints.MapRazorPages();
+                //블레이저 기본
+
 
                 // 루트 페이지 로드하자마자 특정 URL로 이동하고자 할 때 
                 endpoints.MapGet("/", context =>
