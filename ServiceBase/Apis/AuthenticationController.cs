47722a4dcbeed30910a44ff46a3cad3efac7465a
이진영 <codingzombie82@gmail.com>
2021-01-19 18:26:46 +0900

    Jwt 인증 확인용 내용 저장

---
+++ ServiceBase/Apis/AuthenticationController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServiceBase.Data;
+using ServiceBase.Models.Users;
+using ServiceBase.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ServiceBase.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ITokenBuilder _tokenBuilder;
+
+        public AuthenticationController(
+            ApplicationDbContext context,
+            ITokenBuilder tokenBuilder)
+        {
+            _context = context;
+            _tokenBuilder = tokenBuilder;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] User user)
+        {
+            var dbUser = await _context
+                .Users
+                .SingleOrDefaultAsync(u => u.Username == user.Username);
+
+            if (dbUser == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            // This is just an example, made for simplicity; do not store plain passwords in the database
+            // Always hash and salt your passwords
+            var isValid = dbUser.Password == user.Password;
+
+            if (!isValid)
+            {
+                return BadRequest("Could not authenticate user.");
+            }
+
+            var token = _tokenBuilder.BuildToken(user.Username);
+
+            return Ok(token);
+        }
+
+        [HttpGet("verify")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> VerifyToken()
+        {
+            var username = User
+                .Claims
+                .SingleOrDefault();
+
+            if (username == null)
+            {
+                return Unauthorized();
+            }
+
+            var userExists = await _context
+                .Users
+                .AnyAsync(u => u.Username == username.Value);
+
+            if (!userExists)
+            {
+                return Unauthorized();
+            }
+
+            return NoContent();
+        }
+    }
+}
--- ServiceBase/Startup.cs
+++ ServiceBase/Startup.cs
@@ -1,14 +1,19 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using ServiceBase.Data;
+using ServiceBase.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ServiceBase
@@ -26,6 +31,37 @@ namespace ServiceBase
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services
+             .AddAuthentication(options =>
+             {
+                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+             })
+              .AddJwtBearer(cfg =>
+              {
+                  cfg.RequireHttpsMetadata = true;
+                  cfg.SaveToken = true;
+                  cfg.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                  {
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("placeholder-key-that-is-long-enough-for-sha256")),
+                      ValidateAudience = false,
+                      ValidateIssuer = false,
+                      ValidateLifetime = false,
+                      RequireExpirationTime = false,
+                      ClockSkew = TimeSpan.Zero,
+                      ValidateIssuerSigningKey = true
+                  };
+              });
+
+            services.AddScoped<ITokenBuilder, TokenBuilder>();
+
+            services.AddControllers();
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
@@ -49,11 +85,24 @@ namespace ServiceBase
             app.UseStaticFiles();
 
             app.UseRouting();
+            
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapRazorPages();
+
+                // 루트 페이지 로드하자마자 특정 URL로 이동하고자 할 때 
+                endpoints.MapGet("/", context =>
+                {
+                    context.Response.Redirect("/Home");
+                    return Task.CompletedTask;
+                });
             });
         }
     }
